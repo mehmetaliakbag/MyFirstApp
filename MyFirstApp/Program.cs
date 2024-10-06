3019b93abdd0212d25541abc4ce8311311ccbdf2
Mehmet Ali <Mehmet Ali@DESKTOP-4TIU0GP>
2024-10-06 23:13:20 +0300

    Add project files.

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Mehmet Ali Akbağ" +
+                  "\nProgramlamanın temellerini hızlıca öğrenmek ve uygulamalı projelerle " +
+                  "gerçek deneyim kazanmak benim için çok önemli. Yazılım alanında iş bulma " +
+                  "şansımı artırmak da hedeflerim arasında. Diğer katılımcılarla ve sektördeki " +
+                  "profesyonellerle bağlantı kurmak, benim için çok değerli. " +
+                  "Grup içinde öğrenmek motivasyonumu artırır ve birbirimize destek olabiliriz. " +
+                  "Kısa sürede temel becerileri kazanmak, beni bu alanda daha da ileriye taşıyacak. " +
+                  "Kendi projelerimi geliştirerek portföyümü zenginleştirmek istiyorum. " +
+                  "Ayrıca, analitik düşünme ve problem çözme becerilerimi geliştirmek de büyük bir artı.");
